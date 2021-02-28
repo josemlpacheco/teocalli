3bba91f77fa8cb313b72cd5c966a050a05d4f7f6
GerardCod <gerardo.aguilar@alumno.uttehuacan.edu.mx>
2021-02-28 00:10:12 -0600

    [MODIFY] Setting up the SignUpModule to generate the dependencies

--- app/src/main/java/com/example/teocalli/signup/SignUpModule.java
+++ app/src/main/java/com/example/teocalli/signup/SignUpModule.java
@@ -1,8 +1,22 @@
 package com.example.teocalli.signup;
 
 import dagger.Module;
+import dagger.Provides;
 
 @Module
 public class SignUpModule {
 
+    @Provides
+    public SignupMVP.Presenter providePresenter(SignupMVP.Interactor interactor) {
+        return new SignUpPresenter(interactor);
+    }
+
+    @Provides
+    public SignupMVP.Interactor provideInteractor(SignUpRepository repository) {
+        return new SignUpInteractor(repository);
+    }
+
+    public SignUpRepository provideSignUpRepository() {
+        return new ApiSignUpRepository();
+    }
 }
